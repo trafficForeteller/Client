3566d642f35e0f2603e92cc8251542db45684187
unknown <shy6295@ewhain.net>
2022-12-30 18:52:09 +0900

    [ feat ] 랜딩페이지 이미지 첨부

--- naechinso-web/src/@components/LandingPage/index.tsx
+++ naechinso-web/src/@components/LandingPage/index.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
+import { Landing } from "../../asset/image";
 import Title from "../@common/Title";
 
 export default function LandingPage() {
   return (
     <St.LandingPage>
-      <Title title={"어떤 친구인지 너무 기대돼🥰 내친소는 친구의 추천사가 있는 사람만 이용이 가능하거든"} />
+      <Title title={"어떤 친구인지 너무 기대돼🥰"} />
+      <St.Blank />
+      <Title title={"내친소는 친구의 추천사가 있는 사람만 이용이 가능하거든"} />
+      <St.LandingImg src={Landing} alt={"랜딩페이지 아이콘"} />
     </St.LandingPage>
   );
 }
@@ -13,5 +17,13 @@ export default function LandingPage() {
 const St = {
   LandingPage: styled.main`
     position: relative;
+    padding-top: 5.2rem;
+  `,
+  Blank: styled.div`
+    height: 2rem;
+  `,
+  LandingImg: styled.img`
+    height: 35rem;
+    margin-top: 6.3rem;
   `,
 };
