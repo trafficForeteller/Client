a537f76d7ab518c042f5af48ef4fd001601ea231
Hennization <shy6295@ewhain.net>
2023-01-09 13:53:21 +0900

    [ design ] 추천사 랜딩페이지 UI 스타일링

--- naechinso-web/src/@components/RecommendLandingPage/index.tsx
+++ naechinso-web/src/@components/RecommendLandingPage/index.tsx
@@ -5,9 +5,15 @@ import { SubTitle, Title } from "../@common";
 export default function RecommendLandingPage() {
   return (
     <St.RecommendLandingPage>
-      <Title title="친구를 소개해줘!" />
-      <Title title="네 친구라면 분명 멋있겠지?" />
-      <SubTitle subTitle="" />
+      <St.TitleWrapper>
+        <Title title="친구를 소개해줘!" />
+        <Title title="네 친구라면 분명 멋있겠지?" />
+      </St.TitleWrapper>
+      <St.SubTitleWrapper>
+        <SubTitle subTitle="현재는 수도권에 거주하는 89~99년도생만 내친소를 이용할 수 있어!" />
+        <SubTitle subTitle="(추천하는 사람의 나이는 상관없어)" />
+      </St.SubTitleWrapper>
+
       <St.BackgroundColor></St.BackgroundColor>
     </St.RecommendLandingPage>
   );
@@ -16,6 +22,9 @@ export default function RecommendLandingPage() {
 const St = {
   RecommendLandingPage: styled.main`
     height: 100vh;
+
+    display: flex;
+    flex-direction: column;
   `,
   BackgroundColor: styled.div`
     width: 100%;
@@ -24,6 +33,13 @@ const St = {
     bottom: 0;
     transform: rotate(-180deg);
     background: linear-gradient(180deg, #ffe49d 2.78%, #ffffff 100%);
+    z-index: -1;
   `,
-  Title: styled.h1``,
+  TitleWrapper: styled.section`
+    margin: 25.2rem 0 2.6rem 3.5rem;
+  `,
+  SubTitleWrapper: styled.section`
+    width: 30rem;
+    margin: 0 auto;
+  `,
 };
