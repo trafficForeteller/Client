690711db36ae1df23fdcaa9d9c3fecd0f7c93910
Hennization <shy6295@ewhain.net>
2023-01-11 21:13:21 +0900

    [ feat ] Patch 구현

--- naechinso-web/src/apis/recommend.api.ts
+++ naechinso-web/src/apis/recommend.api.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line
-import { IPostFriendsInfo } from "../types/recommend";
+import { IPostFriendInfo } from "../types/recommend";
 import { serverAxios } from ".";
 
 const PREFIX_URL = "/recommend";
 
 export const postRecommendFriendInfo = async (
-  friendsInfo: IPostFriendsInfo,
+  friendsInfo: IPostFriendInfo,
   accessToken: string | null,
 ): Promise<void | null> => {
   const { data } = await serverAxios.post(`${PREFIX_URL}/request-uuid`, friendsInfo, {
@@ -21,4 +21,23 @@ export const postRecommendFriendInfo = async (
     }
     throw new Error("Failed to post your recommend");
   }
+};
+
+export const patchRecommendFriendDetail = async (
+  friendDetail: IPatchFriendDetail,
+  accessToken: string | null,
+  uuid: string | null,
+): Promise<void | null> => {
+  const { data } = await serverAxios.patch(`${PREFIX_URL}/${uuid}/accept`, friendDetail, {
+    headers: { Authorization: `${accessToken}`, "Content-Type": "application/json" },
+  });
+  try {
+    if (data.status === 200) {
+      return data.data;
+    }
+  } catch (err) {
+    if (data.status === 400) {
+      return data.message;
+    }
+    throw new Error("Failed to patch your detail recommend");
 };
--- naechinso-web/src/types/recommend.ts
+++ naechinso-web/src/types/recommend.ts
@@ -1,6 +1,12 @@
-export interface IPostFriendsInfo {
+export interface IPostFriendInfo {
   phone: string;
   name: string;
   period: string;
   meet: string;
+}
+
+export interface IPatchFriendDetail {
+  appealDetail: string;
+  appeals: string[];
+  dontGo: string;
 }
