c672828c498b1c3987a180d63715adf304a81a26
Hennization <shy6295@ewhain.net>
2023-01-12 15:25:58 +0900

    [ fix ] 질문 리스트 수정

--- naechinso-web/src/@components/RecommendPage/index.tsx
+++ naechinso-web/src/@components/RecommendPage/index.tsx
@@ -14,7 +14,13 @@ export default function RecommendPage() {
 
   return (
     <St.Recommend>
-      <FixedHeader header="추천사" progressRate={80} title1="친구를 잘 설명할 수 있는" title2="질문을 골라 답해줘!" />
+      <FixedHeader
+        header="추천사"
+        progressRate={85}
+        title1={questionData.desc1}
+        title2={questionData.desc2}
+        title3={questionData.desc3}
+      />
     </St.Recommend>
   );
 }
