7b87ef2412ed73c260d8b630aa04a347380e1a23
Hennization <shy6295@ewhain.net>
2023-01-13 17:46:20 +0900

    [ design ] 추천인 소개 UI구현

--- naechinso-web/src/@components/@common/ShortInputBox.tsx
+++ naechinso-web/src/@components/@common/ShortInputBox.tsx
@@ -5,7 +5,7 @@ export interface ShortInputProps {
   placeholder: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  isModalOpened: boolean;
+  isModalOpened?: boolean;
   step: number;
 }
 
@@ -13,15 +13,24 @@ export default function ShortInputBox(props: ShortInputProps) {
   const { label, placeholder, value, onChange, isModalOpened, step } = props;
 
   return (
-    <St.InputBox isModalOpened={isModalOpened}>
-      <St.Label step={step}>{label}</St.Label>
-      <St.Input placeholder={placeholder} value={value} onChange={(e) => onChange(e)} />
-    </St.InputBox>
+    <>
+      {isModalOpened ? (
+        <St.InputBox isModalOpened={isModalOpened}>
+          <St.Label step={step}>{label}</St.Label>
+          <St.Input placeholder={placeholder} value={value} onChange={(e) => onChange(e)} />
+        </St.InputBox>
+      ) : (
+        <St.InputWrapper>
+          <St.Label step={step}>{label}</St.Label>
+          <St.Input placeholder={placeholder} value={value} onChange={(e) => onChange(e)} />
+        </St.InputWrapper>
+      )}
+    </>
   );
 }
 
 const St = {
-  InputBox: styled.section<{ isModalOpened: boolean }>`
+  InputBox: styled.section<{ isModalOpened: boolean | null }>`
     width: 33.5rem;
     height: 8rem;
 
@@ -31,6 +40,16 @@ const St = {
     margin: 1.6rem auto 0;
     position: relative;
     z-index: ${({ isModalOpened }) => (isModalOpened ? "-1" : "")};
+  `,
+  InputWrapper: styled.section`
+    width: 33.5rem;
+    height: 8rem;
+
+    border-radius: 16px;
+    background-color: ${({ theme }) => theme.colors.neural};
+    padding: 1rem 2rem 1.6rem;
+    margin: 1.6rem auto 0;
+    position: relative;
   `,
   Label: styled.p<{ step: number }>`
     color: ${({ theme, step }) => (step === 1 ? theme.colors.orange : theme.colors.gray40)};
--- naechinso-web/src/@components/JobPage/index.tsx
+++ naechinso-web/src/@components/JobPage/index.tsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { IcSheild } from "../../asset/icons";
-import { FixedHeader } from "../@common";
+import { routePaths } from "../../core/routes/path";
+import { FixedHeader, ShortInputBox } from "../@common";
 
 export default function JobPage() {
+  const [step, setStep] = useState(1);
+  const [activeBtn, setActiveBtn] = useState(false);
+  const navigate = useNavigate();
+
+  const [job, setJob] = useState("");
+  const [jobGroup, setJobGroup] = useState("");
+
+  useEffect(() => {
+    // step에 따라 다른 모달 open
+    if (step === 3) {
+      navigate(`${routePaths.Keyword}`);
+    }
+  }, [step]);
+
+  useEffect(() => {
+    // step에 따른 ActiveButton 활성화
+    if (job.length > 0) setActiveBtn(true);
+    else if (step >= 2 && jobGroup.length > 0) setActiveBtn(true);
+    else setActiveBtn(false);
+  }, [job, jobGroup]);
+
+  const handleJobInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 직업을 관리하는 함수
+    setJob(e.target.value);
+    console.log(job);
+  };
+
+  const handleJobGroupInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 직업을 관리하는 함수
+    setJobGroup(e.target.value);
+    console.log(jobGroup);
+  };
+
+  const handleStep = () => {
+    // 친구정보 step을 관리하는 함수
+    setStep(step + 1);
+    setActiveBtn(false);
+  };
+
   return (
     <St.Job>
       <FixedHeader
@@ -21,6 +63,32 @@ export default function JobPage() {
           <St.SheildExample>ex. 한국은행 =&gt; 금융공기업 / 당근마켓 =&gt; 중고거래 스타트업</St.SheildExample>
         </St.SheildWrapper>
       </St.SheildBox>
+
+      {step >= 2 ? (
+        <ShortInputBox
+          label="직무"
+          placeholder="무슨 일을 하고있어?"
+          value={jobGroup}
+          onChange={(e) => handleJobGroupInput(e)}
+          step={step}
+        />
+      ) : (
+        <></>
+      )}
+
+      <ShortInputBox
+        label="직장"
+        placeholder="재직중인 회사명을 적어줘"
+        value={job}
+        onChange={(e) => handleJobInput(e)}
+        step={step}
+      />
+
+      <St.NextStepBtnWrapper>
+        <St.NextStepBtn type="button" disabled={!activeBtn} onClick={handleStep}>
+          다음
+        </St.NextStepBtn>
+      </St.NextStepBtnWrapper>
     </St.Job>
   );
 }
@@ -47,5 +115,27 @@ const St = {
   SheildExample: styled.p`
     color: ${({ theme }) => theme.colors.gray40};
     ${({ theme }) => theme.fonts.caption3}
+  `,
+  NextStepBtnWrapper: styled.section`
+    display: flex;
+    justify-content: center;
+  `,
+  NextStepBtn: styled.button`
+    position: fixed;
+
+    bottom: 3.5rem;
+    padding: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.orange};
+    color: ${({ theme }) => theme.colors.white};
+    ${({ theme }) => theme.fonts.sub3};
+    width: 33.5rem;
+    height: 5.6rem;
+    border-radius: 1.6rem;
+
+    &:disabled {
+      background-color: ${({ theme }) => theme.colors.orange20};
+      cursor: default;
+    }
   `,
 };
