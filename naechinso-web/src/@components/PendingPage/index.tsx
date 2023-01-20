24faa36ceff4900397959399c1724ad577e46d55
Hennization <shy6295@ewhain.net>
2023-01-20 16:24:35 +0900

    [ feat ] 추천사 수정 완료 페이지 구현

--- naechinso-web/src/@components/PendingPage/index.tsx
+++ naechinso-web/src/@components/PendingPage/index.tsx
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
-import { ImgRecommendLanding } from "../../asset/image";
+import { ImgCommentNaechinso, ImgHandsUp } from "../../asset/image";
 
 export default function PendingPage() {
   return (
     <St.PendingPage>
-      <St.Naechinso src={ImgRecommendLanding} alt="내친소" />
+      <St.Naechinso src={ImgCommentNaechinso} alt="내친소" />
+      <St.CommentBox>
+        <St.CommentWrapper>
+          <St.Comment>추천사 작성이 완료 됐어!</St.Comment>
+          <St.Comment>친구를 추천해줘서</St.Comment>
+          <St.Comment>정말 고마워!💕</St.Comment>
+          <St.Comment>
+            믿고 추천한만큼
+            <br />
+            내친소도 정말 열심히 할게!
+          </St.Comment>
+        </St.CommentWrapper>
+
+        <St.Emoji src={ImgHandsUp} alt="손 올리는 아이콘" />
+      </St.CommentBox>
     </St.PendingPage>
   );
 }
@@ -14,7 +28,37 @@ const St = {
   PendingPage: styled.main`
     width: 100%;
     height: 100%;
-    background-color: ${({ theme }) => theme.colors.orange};
+    background-color: ${({ theme }) => theme.colors.neural};
+
+    display: flex;
+    gap: 1.5rem;
+    padding-top: 20%;
   `,
-  Naechinso: styled.img``,
+  Naechinso: styled.img`
+    width: 12.5rem;
+    height: 16.5rem;
+  `,
+  CommentBox: styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+  `,
+  CommentWrapper: styled.article`
+    margin-top: 20%;
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+  `,
+  Comment: styled.p`
+    width: fit-content;
+    background-color: ${({ theme }) => theme.colors.white};
+    padding: 0.8rem 1.2rem;
+    color: ${({ theme }) => theme.colors.black};
+    ${({ theme }) => theme.fonts.sub3};
+    border-radius: 0px 16px 16px 16px;
+  `,
+  Emoji: styled.img`
+    width: 9rem;
+    height: 10rem;
+  `,
 };
