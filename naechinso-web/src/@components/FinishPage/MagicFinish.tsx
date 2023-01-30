91a6410f195fc52c4ed7449229be3fa4fe8799fd
Hennization <shy6295@ewhain.net>
2023-01-30 14:53:50 +0900

    [ feat ] JS Key값 .env 파일에 넣기

--- naechinso-web/src/@components/FinishPage/MagicFinish.tsx
+++ naechinso-web/src/@components/FinishPage/MagicFinish.tsx
@@ -7,7 +7,7 @@ import { ImgCommentNaechinso, ImgHandsUp } from "../../asset/image";
 export default function MagicFinish() {
   useEffect(() => {
     if (!window.Kakao.isInitialized()) {
-      window.Kakao.init("9774e93798abb4a17249f0572b739887");
+      window.Kakao.init(`${process.env.REACT_APP_JS_KEY}`);
       console.log(window.Kakao.isInitialized());
     }
   }, []);
