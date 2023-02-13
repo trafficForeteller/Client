93570d47cd2a4a983eb8df7a333ed16207d1909b
Hennization <shy6295@ewhain.net>
2023-02-13 11:31:12 +0900

    [ fix ] 토큰 들어온 후 아무 정보 없으면 recommend landing

--- naechinso-web/src/@components/CertifiedPage/index.tsx
+++ naechinso-web/src/@components/CertifiedPage/index.tsx
@@ -106,6 +106,7 @@ export default function CertifiedPage(props: CertifiedPageProps) {
         } else if (userData[0].pendingStatus === "reject" && userData[0].type === "EDU") {
           navigate(`${routePaths.EduEdit}`, { state: userData[0] });
         } else if (!userData[0]) navigate(`${routePaths.RecommendLanding}`);
+        else navigate(`${routePaths.RecommendLanding}`);
       } else if (localStorage.getItem("member-uuid") === "/edit") navigate(`${routePaths.EditRecommender}`);
       else navigate(`${routePaths.RecommendLanding}`);
     }
