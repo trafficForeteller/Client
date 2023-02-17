ed6e758b043ee1ad9a78be8f08a48c4834ae34f5
Hennization <shy6295@ewhain.net>
2023-02-17 18:06:36 +0900

    [ fix ] baseURL string으로 수정

--- naechinso-web/src/apis/index.ts
+++ naechinso-web/src/apis/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const serverAxios = axios.create({
   // 실 서버 사용
-  baseURL: process.env.REACT_APP_API,
+  baseURL: "https://api.naechinso.com",
 
   // 개발 서버 사용
   // baseURL: process.env.REACT_APP_DEV_API,
