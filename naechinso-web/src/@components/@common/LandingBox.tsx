d64acb1ef87a02405891239b40b4d287eb6e8949
Hennization <shy6295@ewhain.net>
2023-02-23 16:27:45 +0900

    [ feat ] /edit으로 진입 시 member-uuid로 저장

--- naechinso-web/src/@components/@common/LandingBox.tsx
+++ naechinso-web/src/@components/@common/LandingBox.tsx
@@ -29,9 +29,9 @@ export default function LandingBox(props: LandingBoxProps) {
     localStorage.removeItem("postRecommender");
     localStorage.removeItem("genderTypeList");
     localStorage.removeItem("uuid");
-    localStorage.removeItem("member-uuid");
 
-    if (location.pathname.length === 37) localStorage.setItem("member-uuid", location.pathname);
+    if (location.pathname.length === 37 || location.pathname === "/edit")
+      localStorage.setItem("member-uuid", location.pathname);
     else localStorage.removeItem("member-uuid");
     localStorage.getItem("accessToken") && setAccessToken(true);
   }, [location]);
