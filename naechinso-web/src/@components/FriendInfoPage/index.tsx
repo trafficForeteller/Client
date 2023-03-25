3325ff0428f2d7260d8b2cd48c199c63ba934935
Hennization <shy6295@ewhain.net>
2023-03-25 14:33:59 +0900

    [ feat ] 내 친구이름 수정

--- naechinso-web/src/@components/FriendInfoPage/index.tsx
+++ naechinso-web/src/@components/FriendInfoPage/index.tsx
@@ -333,17 +333,7 @@ export default function FriendInfoPage() {
         <></>
       )}
 
-      {memberName ? (
-        <ShortInputBox
-          label="내 친구 이름"
-          placeholder="친구 이름을 실명으로 적어줘"
-          value={memberName}
-          onChange={handleNameInput}
-          isModalOpened={isModalOpened}
-          step={step}
-          handleStep={handleStep}
-        />
-      ) : (
+      {memberName && (
         <ShortInputBox
           label="내 친구 이름"
           placeholder="친구 이름을 실명으로 적어줘"
