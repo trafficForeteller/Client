4fea1d3531201432090621792342ab11f41cec90
Hennization <shy6295@ewhain.net>
2023-04-17 11:49:11 +0900

    [ fix ] navigator 객체로 클립보드 복사 구현

--- naechinso-web/src/@components/FinishPage/Finish.tsx
+++ naechinso-web/src/@components/FinishPage/Finish.tsx
@@ -13,7 +13,7 @@ export default function Finish() {
   }, []);
 
   const shareRecommendLink = () => {
-    const recommenderName = localStorage.getItem("recommenderName");
+    const recommenderName = localStorage.getItem("recommenderName") || "친한";
 
     if (Mobile() && navigator.share) {
       navigator
@@ -29,13 +29,14 @@ export default function Finish() {
         .then(() => console.log("공유 성공"))
         .catch((error) => console.log("공유 실패", error));
     } else {
-      window.navigator.clipboard.writeText(`${recommenderName} 친구가 너에 대한 추천사 작성을 완료했어!🎉 
+      navigator.clipboard.writeText(`${recommenderName} 친구가 너에 대한 추천사 작성을 완료했어!🎉 
+      
 내친소는 너처럼 실제 친구에게 추천을 받은, 주변에서 신뢰받고 애정받은 사람들만 가입할 수 있는 지인소개팅 서비스야! (너는 복받았다! 이런 좋은 친구를 두다니!) 
       
 이제 너가 할 일은 간단한 자기소개만 하면 끝!😎 내친소에서 너만큼 멋진 친구들을 만나러 가볼까?
       
 앱 다운로드: https://recommend.naechinso.com/arrive`);
-      alert("클립보드에 초대링크를 복사했어요!");
+      alert("클립보드에 초대링크를 복사했어!");
     }
   };
 
