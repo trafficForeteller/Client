70b99342949da93929683dd611818db46391144c
Hennization <shy6295@ewhain.net>
2023-05-22 11:23:51 +0900

    [ feat ] 랜딩페이지 이전 버전으로 수정

---
+++ naechinso-web/src/core/recommender/recommender.ts
@@ -0,0 +1,37 @@
+import { ImgCommentNaechinso } from "../../asset/image";
+
+export type recommendationExplainProps = {
+  id: number;
+  title: string;
+  desc1: string;
+  desc2: string;
+  desc3: string;
+  img: any;
+};
+
+export const recommendationExplainList: recommendationExplainProps[] = [
+  {
+    id: 0,
+    title: "커플이 되면 치킨을 쏠게!",
+    desc1: "sdfdfsfsdfsdf",
+    desc2: "sdfsdffjwherjkwef",
+    desc3: "sdkfsjkldfjkldfjsd",
+    img: ImgCommentNaechinso,
+  },
+  {
+    id: 1,
+    title: "1커플이 되면 치킨을 쏠게!",
+    desc1: "sdfdfsfsdfsdf",
+    desc2: "sdfsdffjwherjkwef",
+    desc3: "sdkfsjkldfjkldfjsd",
+    img: ImgCommentNaechinso,
+  },
+  {
+    id: 2,
+    title: "2커플이 되면 치킨을 쏠게!",
+    desc1: "sdfdfsfsdfsdf",
+    desc2: "sdfsdffjwherjkwef",
+    desc3: "sdkfsjkldfjkldfjsd",
+    img: ImgCommentNaechinso,
+  },
+];
