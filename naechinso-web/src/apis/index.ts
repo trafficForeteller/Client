7922b6e0d76a1b1c5b3c0dd23954596faa5265f9
Hennization <shy6295@ewhain.net>
2023-09-20 17:17:58 +0900

    [ feat ] 실서버 주소 변경

--- naechinso-web/src/apis/index.ts
+++ naechinso-web/src/apis/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const serverAxios = axios.create({
   // 실 서버 사용
-  baseURL: "https://api.naechinso.com",
+  baseURL: "https://apiv1.naechinso.com",
 
   // 개발 서버 사용
   // baseURL: "https://dev.naechinso.com",
